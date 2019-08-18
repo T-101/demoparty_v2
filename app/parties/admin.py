683fe3ad54a9dbbabf891c3384853fe3e906cd02
T-101 <t-101@t-101.org>
2019-08-18 14:15:31 +0300

    db changes

--- app/parties/admin.py
+++ app/parties/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from parties.models import DemoParty, DemoPartyLocation, DemoPartyURL, DemoPartyVisitor, DemoPartySeries
+
+class DemoPartyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'series', 'location', 'url', 'demo_party_start', 'demo_party_end']
+    search_fields = ['name', 'series__name', 'url__url']
+
+admin.site.register(DemoParty, DemoPartyAdmin)
+
+class DemoPartySeriesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+admin.site.register(DemoPartySeries, DemoPartySeriesAdmin)
+
+class DemoPartyLocationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'address1', 'address2', 'postal_code', 'city', 'country', 'latitude', 'longitude']
+
+admin.site.register(DemoPartyLocation, DemoPartyLocationAdmin)
+
+class DemoPartyURLAdmin(admin.ModelAdmin):
+    list_display = ['id', 'url']
+
+admin.site.register(DemoPartyURL, DemoPartyURLAdmin)
+
+class DemoPartyVisitorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'demo_party', 'status']
+
+admin.site.register(DemoPartyVisitor, DemoPartyVisitorAdmin)
--- app/parties/models.py
+++ app/parties/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
-# Create your models here.
+from authentication.models import DemoPartyUser
+
+
+class DemoPartyLocation(models.Model):
+    address1 = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255, blank=True, null=True)
+    postal_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+    latitude = models.DecimalField(
+        max_digits=32, decimal_places=29, blank=True, null=True)
+    longitude = models.DecimalField(
+        max_digits=32, decimal_places=29, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.address1}, {self.city}, {self.country}"
+
+
+class DemoPartyURL(models.Model):
+    url = models.URLField()
+
+    def __str__(self):
+        return self.url
+
+
+class DemoPartySeries(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class DemoParty(models.Model):
+    name = models.CharField(max_length=255)
+    series = models.ForeignKey(DemoPartySeries, on_delete=models.CASCADE, related_name='parties')
+
+    location = models.ForeignKey(
+        DemoPartyLocation, on_delete=models.CASCADE, related_name='demo_parties', blank=True, null=True)
+    url = models.ForeignKey(DemoPartyURL, on_delete=models.CASCADE,
+                            related_name='demo_parties', blank=True, null=True)
+
+    demo_party_start = models.DateField()
+    demo_party_end = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.demo_party_start.year})"
+    
+    def save(self, *args, **kwargs):
+        # TODO strip year from party name if matches with demo_party_start.year
+        return super().save(*args, **kwargs)
+
+class DemoPartyVisitor(models.Model):
+    NOT_GOING = 0
+    PLANNING = 1
+    WENT = 2
+
+    CHOICES = [
+        (NOT_GOING, "Not going"),
+        (PLANNING, "Planning"),
+        (WENT, "Went")
+    ]
+
+    user = models.OneToOneField(DemoPartyUser, on_delete=models.CASCADE)
+    demo_party = models.ForeignKey(DemoParty, on_delete=models.CASCADE, related_name='visitors')
+    status = models.IntegerField(choices=CHOICES, default=PLANNING)
+
+    def __str__(self):
+        return f"{self.user.username} at {self.demo_party.name}"
