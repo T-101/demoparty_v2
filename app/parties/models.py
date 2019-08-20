6e1b72763943e4efa5ac8fc75bff27d2a0a7a811
Antti Rummukainen <t-101@t-101.org>
2019-08-21 00:33:23 +0300

    datetimestamps, augmented models, whatnots

--- app/parties/models.py
+++ app/parties/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 from authentication.models import DemoPartyUser
 
 
+class TimeStampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
 class DemoPartyLocation(models.Model):
-    address1 = models.CharField(max_length=255)
-    address2 = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    address_extra = models.CharField(max_length=255, blank=True, null=True)
     postal_code = models.CharField(max_length=10)
     city = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
@@ -16,10 +26,10 @@ class DemoPartyLocation(models.Model):
         max_digits=32, decimal_places=29, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.address1}, {self.city}, {self.country}"
+        return f"{self.name}, {self.city}, {self.country}"
 
 
-class DemoPartyURL(models.Model):
+class DemoPartyExternalURL(models.Model):
     url = models.URLField()
 
     def __str__(self):
@@ -29,28 +39,39 @@ class DemoPartyURL(models.Model):
 class DemoPartySeries(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name_plural = "Demo Party series"
+
     def __str__(self):
         return self.name
 
 
-class DemoParty(models.Model):
+class DemoParty(TimeStampModel):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(blank=True, null=True)
     series = models.ForeignKey(DemoPartySeries, on_delete=models.CASCADE, related_name='parties')
 
     location = models.ForeignKey(
         DemoPartyLocation, on_delete=models.CASCADE, related_name='demo_parties', blank=True, null=True)
-    url = models.ForeignKey(DemoPartyURL, on_delete=models.CASCADE,
-                            related_name='demo_parties', blank=True, null=True)
+    url = models.URLField()
+    external_urls = models.ManyToManyField(DemoPartyExternalURL, related_name='demo_parties')
 
     demo_party_start = models.DateField()
     demo_party_end = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} ({self.demo_party_start.year})"
-    
+
     def save(self, *args, **kwargs):
         # TODO strip year from party name if matches with demo_party_start.year
+        if not self.demo_party_end:
+            self.demo_party_end = self.demo_party_start
+        if not self.slug:
+            slug = slugify('%s %s' % (self.name, self.demo_party_start.year))
+            self.slug = re.sub(r'-(\d{4})(?=.+\1)', '', slug)
+
         return super().save(*args, **kwargs)
+
 
 class DemoPartyVisitor(models.Model):
     NOT_GOING = 0
