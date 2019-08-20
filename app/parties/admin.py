8a4f42297b23b8c5fe1d42a17cfba22390f69534
Antti Rummukainen <t-101@t-101.org>
2019-08-21 00:32:56 +0300

    updated admin views

--- app/parties/admin.py
+++ app/parties/admin.py
@@ -1,29 +1,40 @@
 from django.contrib import admin
 
-from parties.models import DemoParty, DemoPartyLocation, DemoPartyURL, DemoPartyVisitor, DemoPartySeries
+from parties.models import DemoParty, DemoPartyLocation, DemoPartyExternalURL, DemoPartyVisitor, DemoPartySeries
+
 
 class DemoPartyAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'series', 'location', 'url', 'demo_party_start', 'demo_party_end']
+    list_display = ['id', 'name', 'series', 'slug', 'location', 'url', 'demo_party_start', 'demo_party_end', 'created_at',
+                    'updated_at']
     search_fields = ['name', 'series__name', 'url__url']
 
+
 admin.site.register(DemoParty, DemoPartyAdmin)
+
 
 class DemoPartySeriesAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
 
+
 admin.site.register(DemoPartySeries, DemoPartySeriesAdmin)
 
+
 class DemoPartyLocationAdmin(admin.ModelAdmin):
-    list_display = ['id', 'address1', 'address2', 'postal_code', 'city', 'country', 'latitude', 'longitude']
+    list_display = ['id', 'address', 'address_extra', 'postal_code', 'city', 'country', 'latitude', 'longitude']
+
 
 admin.site.register(DemoPartyLocation, DemoPartyLocationAdmin)
 
-class DemoPartyURLAdmin(admin.ModelAdmin):
+
+class DemoPartyExternalURLAdmin(admin.ModelAdmin):
     list_display = ['id', 'url']
 
-admin.site.register(DemoPartyURL, DemoPartyURLAdmin)
+
+admin.site.register(DemoPartyExternalURL, DemoPartyExternalURLAdmin)
+
 
 class DemoPartyVisitorAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'demo_party', 'status']
+
 
 admin.site.register(DemoPartyVisitor, DemoPartyVisitorAdmin)
